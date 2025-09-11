5e91074c8e89c79f6499f2fbf27b1000d30bd29b
puspanDas <puspakdas123@hotmail.com>
2025-09-12 00:05:09 +0530

    Delete medical report-analyzer directory

--- medical report-analyzer/start_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Healthcare MediaAnalytics Pro...
-echo.
-echo Installing dependencies...
-pip install -r requirements.txt
-echo.
-echo Starting Flask application...
-python app.py
-pause
